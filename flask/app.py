d3544f769d5f151ec7e0e0b18fb482e05ad60cf2
Juan Roldan <juanroldan1989@gmail.com>
2024-10-13 22:33:44 +0200

    Flask app - Action caching implemented

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from flask_caching import Cache
 from db import initialize_pool
 from db_queries import get_locations, get_average_temperature
 from cache import cache_query
@@ -7,14 +8,21 @@ import os
 
 app = Flask(__name__)
 
+app.config['CACHE_TYPE'] = 'redis'
+app.config['CACHE_REDIS_HOST'] = os.environ.get('REDIS_HOST')
+app.config['CACHE_REDIS_PORT'] = os.environ.get('REDIS_PORT')
+
 # Initialize the connection pool
 initialize_pool() if os.environ.get('USE_POOLING') == 'true' else None
+cache = Cache(app, config={'CACHE_TYPE': 'redis'})
 
 @app.route('/')
+@cache.cached(timeout=300) # Cache the query for 5 minutes
 def index():
   locations = cache_query('locations', get_locations)
   return render_template('index.html', locations=locations)
 
+@cache.cached(timeout=300, query_string=True) # Cache query results based on request parameters for 5 minutes
 @app.route('/query', methods=['POST'])
 def query():
   location = request.form.get('location')
