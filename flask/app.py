dfd52da741b7739cc2e54120832cf34a8b1b9fec
Juan Roldan <juanroldan1989@gmail.com>
2024-10-13 14:22:24 +0200

    DB Connection Pooling implemented
    - USE_POOLING env variable added
    - `db.py` file added within `flask` app to handle all connection_pooling logic
    - README updated with load testing scenario

--- flask/app.py
+++ flask/app.py
@@ -1,32 +1,24 @@
 from flask import Flask, render_template, request, jsonify
-import psycopg2
+from db import get_db_connection, release_db_connection, close_connection_pool, initialize_pool
 import os
 
 app = Flask(__name__)
 
-# Database connection
-def get_db_connection():
-  try:
-    conn = psycopg2.connect(
-      host=os.environ.get('POSTGRES_HOST', 'localhost'),
-      database=os.environ.get('POSTGRES_DB', 'mydb'),
-      user=os.environ.get('POSTGRES_USER', 'user'),
-      password=os.environ.get('POSTGRES_PASSWORD', 'password')
-    )
-    return conn
-  except psycopg2.OperationalError as e:
-    print(f"Error connecting to the database: {e}")
-    return None
+# Initialize the connection pool
+initialize_pool() if os.environ.get('USE_POOLING') == 'true' else None
 
 @app.route('/')
 def index():
   conn = get_db_connection()
+  if conn is None:
+    return "Error connecting to the database", 500
+
   cur = conn.cursor()
   # Fetch 10 distinct locations from the database
   cur.execute("SELECT DISTINCT location FROM weather_data LIMIT 10")
   locations = [row[0] for row in cur.fetchall()]
   cur.close()
-  conn.close()
+  release_db_connection(conn)
 
   # Pass locations to the template
   return render_template('index.html', locations=locations)
@@ -34,14 +26,24 @@ def index():
 @app.route('/query', methods=['POST'])
 def query():
   location = request.form.get('location')
+  if location is None or location == '':
+    return "No location specified", 500
+
   conn = get_db_connection()
+  if conn is None:
+    return "Error connecting to the database", 500
+
+  print(f"Fetching data for location: {location}")
+
   cur = conn.cursor()
   cur.execute("SELECT AVG(temperature) FROM weather_data WHERE location = %s", (location,))
   average_temperature = cur.fetchone()[0]
   cur.close()
-  conn.close()
+  release_db_connection(conn)
 
-  return render_template('results.html', location=location, avg_temp=f"{average_temperature:.2f}")
+  average_temperature = f"{average_temperature:.2f} °C" if average_temperature is not None else "No data available"
+
+  return render_template('results.html', location=location, avg_temp=average_temperature)
 
 @app.route('/health', methods=['GET'])
 def health():
